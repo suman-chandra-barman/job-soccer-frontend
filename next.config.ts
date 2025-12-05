0fb1cdedd056cdd11811ab150cfed016213a5169
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-12-05 17:09:17 +0600

    fix: update image protocol to https and ensure hostname is correctly set in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_HOSTNAME || "10.10.12.125",
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_HOSTNAME as string,
       },
       {
         protocol: "https",
