22cc3c8a199fe3edb300160a30550c7d0eb6508b
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-12-12 10:43:47 +0600

    Update bg color

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -49,7 +49,7 @@ export function Features() {
 
   return (
     <section className="bg-[#F7F6F2]">
-      <div className="py-8 text-center bg-[#FFF8CC]">
+      <div className="py-8 text-center bg-primary">
         <h2 className="text-2xl md:text-4xl font-bold text-center text-gray-900">
           Why is Jobsoccer unique?
         </h2>
